c2f79268f6375c833613c9b300cf7a5c51a5c2e5
IZacarias808 <izacarias2986@myhta.org>
2017-03-10 14:29:00 -1000

    Y U DO DIS

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 function fortuneTeller(){
-
+	var numOfKids = 0
+	var partnerName = "2017 Nissan GTR";
+	var geographicLocation = "San Diego, CA";
+	var jobTitle = "Youtuber";
+	alert ("You will be a " + jobTitle + " in " + geographicLocation + ", and married to " + partnerName + " with " + numOfKids + " kids.")
 }
+
+fortuneTeller();
 
 function ageCalculator(){
 
